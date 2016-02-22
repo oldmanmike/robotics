11817b7a1720898a91588d3afb2712fabf182716
Michael Carpenter <mcarpenter.dev@gmail.com>
2016-02-22 16:17:29 -0500

    Add initial sorter code

---
+++ sorter.py
@@ -0,0 +1,34 @@
+#!python
+
+import sys
+
+from jaraco.nxt import Connection
+from jaraco.nxt import messages
+from jaraco.nxt import _enum as enum
+from jaraco.nxt import locator
+from jaraco.nxt.routine import (
+	cycle_motor_a,
+)
+import time
+
+def querySorter(conn,port):
+	conn.send(messages.GetInputValues(port))
+	raw = conn.recieve()
+	dat = ', '.join('%d' % getattr(input_res, field) for field in input_res.fields)
+	sys.stdout.write(dat)
+
+def runSorter():
+	conn = Connection('/dev/tty.NXT49-DevB')
+	port = enum.InputPort(1)
+	print(port)
+	conn.send(messages.SetInputMode(
+		port,
+		messages.SensorType.switch,
+		messages.SensorMode.boolean,
+	))
+	print(', '.join(field[:4] for field in messages.InputValues.fields))
+	querySorter(conn,port)
+	#cycle_motor_a(conn)
+	conn.close()
+
+__name__ == '__main__' and runSorter()
