6822d5e2c9af574f9b1f1e6cc2d604871604baf9
oldmanmike <oldmanmike.dev@gmail.com>
2016-04-07 14:39:45 -0400

    Add curved line follower

--- hs-robotics/bin/line-follower.hs
+++ hs-robotics/bin/line-follower.hs
@@ -10,13 +10,13 @@ import Bot.Logic
 
 runBot :: Logger -> IO ()
 runBot logger = do
-    withNXT defaultDevice $ 
-      initialLeap logger >> (forever $ followLineWith logger)
+    withNXT defaultDevice $ initialLeap logger >> (forever $ followLineWith logger)
+
 
 initialLeap :: Logger -> NXT ()
 initialLeap logger = do
-    setOutputStateConfirm A 50 [MotorOn,Brake,Regulated] RegulationModeIdle 0 MotorRunStateRunning 0
-    setOutputStateConfirm B 50 [MotorOn,Brake,Regulated] RegulationModeIdle 0 MotorRunStateRunning 0
+    setOutputStateConfirm A 30 [MotorOn,Brake,Regulated] RegulationModeIdle 0 MotorRunStateRunning 0
+    setOutputStateConfirm B 30 [MotorOn,Brake,Regulated] RegulationModeIdle 0 MotorRunStateRunning 0
     liftIO $ threadDelay 1000000
     liftIO $ writeTo logger Info "Beginning line follow..."
 
--- hs-robotics/src/Bot/Logic.hs
+++ hs-robotics/src/Bot/Logic.hs
@@ -15,6 +15,14 @@ import Robotics.NXT
 import Bot.Logger
 import System.Exit
 
+data Cont = KeepGoing | PleaseStop deriving (Show,Eq)
+
+data LineState
+  = LineCenter
+  | LineRight
+  | LineLeft
+  | LineLost
+  deriving (Show,Eq)
 
 calibrateMAX :: InputPort -> Int -> NXT ScaledValue
 calibrateMAX port n = do
@@ -50,37 +58,93 @@ readSensor port sType sMode = do
   resetInputScaledValue port
   return reading
 
+
 maybeTurn :: Logger -> Int -> Int -> NXT ()
 maybeTurn logger readingLeft readingRight
-  | (readingLeft <= 45) || (readingRight <= 45) = do
+  | (readingLeft >= 60) || (readingRight >= 60) = do
       liftIO $ writeTo logger Debug $ "Stopping"
-      rotateMotor A (-10)
-      rotateMotor B (-10)
+      rotateMotor A (30)
+      rotateMotor B (30)
       stopEverything
       shutdown
       liftIO exitSuccess
+      return ()
+  | (readingLeft <= 43) || (readingRight <= 43) = do
+      rotateMotor A 20
+      rotateMotor B 20
+      carefulFollow logger
   | abs (readingLeft - readingRight) <= 2 = do
-      rotateMotor A 25
-      rotateMotor B 25
-  | abs (readingLeft - readingRight) > 2 = do
+      rotateMotor A 30
+      rotateMotor B 30
+      return ()
+  | (abs (readingLeft - readingRight) > 2) && (not (abs (readingLeft - readingRight) > 12)) = do
     if readingLeft > readingRight
       then do
         -- Right side is detecting the line
-        liftIO $ writeTo logger Debug $ "--> Turn Left"
-        rotateMotor A 28
-        rotateMotor B 26
+        liftIO $ writeTo logger Debug $ "--> Turn Right"
+        rotateMotor A 45
+        rotateMotor B 15
+        return ()
       else do
         -- Left side is detecting the line
-        liftIO $ writeTo logger Debug $ "--> Turn Right"
-        rotateMotor A 26
-        rotateMotor B 28
+        liftIO $ writeTo logger Debug $ "--> Turn Left"
+        rotateMotor A 15
+        rotateMotor B 45
+        return ()
   | otherwise = return ()
 
 
-followLineWith :: Logger -> NXT ()
-followLineWith logger = do
-    reading1 <- calibrateAVG Three 3
-    reading2 <- calibrateAVG Four 3
+carefulTurn :: Logger -> Int -> Int -> NXT ()
+carefulTurn logger readingLeft readingRight
+  | (readingLeft >= 60) || (readingRight >= 60) = do
+      liftIO $ writeTo logger Debug $ "Stopping"
+      rotateMotor A (30)
+      rotateMotor B (30)
+      stopEverything
+      shutdown
+      liftIO exitSuccess
+      return ()
+      {-
+  | abs (readingLeft - readingRight) <= 2 = do
+      liftIO $ writeTo logger Debug $ "--> Careful Go Straight"
+      rotateMotor A 20
+      rotateMotor B 20
+      return ()
+      -}
+  | (abs (readingLeft - readingRight) > 2) = do
+    if readingLeft > readingRight
+      then do
+        -- Right side is detecting the line
+        liftIO $ writeTo logger Debug $ "--> Careful Turn Right"
+        rotateMotor A 25
+        rotateMotor B 15
+        return ()
+      else do
+        -- Left side is detecting the line
+        liftIO $ writeTo logger Debug $ "--> Careful Turn Left"
+        rotateMotor A 15
+        rotateMotor B 25
+        return ()
+  | otherwise = return ()
+
+carefulFollow :: Logger -> NXT ()
+carefulFollow logger = do
+    reading1 <- calibrateAVG One 3
+    reading2 <- calibrateAVG Two 3
     liftIO $ writeTo logger Debug $ "Right Reading is: " ++ show reading1
     liftIO $ writeTo logger Debug $ "Left Reading is: " ++ show reading2
-    maybeTurn logger reading1 reading2 
+    liftIO $ writeTo logger Debug $ "Difference is: " ++ show (abs (reading1 - reading2)) 
+    maybeTurn logger reading1 reading2
+    if (not (reading1 <= 43) || (reading1 <= 43))
+      then return ()
+      else carefulFollow logger
+    
+
+followLineWith :: Logger -> NXT ()
+followLineWith logger = do
+    reading1 <- calibrateAVG One 1
+    reading2 <- calibrateAVG Two 1
+    liftIO $ writeTo logger Debug $ "Right Reading is: " ++ show reading1
+    liftIO $ writeTo logger Debug $ "Left Reading is: " ++ show reading2
+    liftIO $ writeTo logger Debug $ "Difference is: " ++ show (abs (reading1 - reading2)) 
+    maybeTurn logger reading1 reading2
